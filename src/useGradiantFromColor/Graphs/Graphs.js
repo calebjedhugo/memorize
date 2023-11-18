a1c822762e2f35bf4c4fe4ff757f3b66f35fc024
Caleb Hugo <calebjedhugo@gmail.com>
2023-11-18 12:11:03 -0500

    Created Graphs

---
+++ src/useGradiantFromColor/Graphs/Graphs.js
@@ -0,0 +1,3 @@
+const Graphs = () => null;
+
+export default Graphs;
---
+++ src/useGradiantFromColor/Graphs/Graphs.stories.js
@@ -0,0 +1,13 @@
+import Graphs from '.';
+
+const Template = (props) => <Graphs {...props} />;
+
+const Example = Template.bind({});
+
+const story = {
+  component: Graphs,
+  title: 'useGradiantFromColor/Graphs',
+};
+
+export { Example };
+export default story;
