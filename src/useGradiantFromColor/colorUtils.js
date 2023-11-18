a9e28484e13d7eca3e32f985eed6ac02a78117e8
Caleb Hugo <calebjedhugo@gmail.com>
2023-11-18 11:56:21 -0500

    Added colorUtils

---
+++ src/useGradiantFromColor/colorUtils.js
@@ -0,0 +1,26 @@
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
+}
+
+function rgbToHex({ r, g, b }) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+const ensureLeadingHex = (hex = '') => {
+  if (hex[0] !== '#') return `#${hex}`;
+  return hex;
+};
+
+export { ensureLeadingHex, hexToRgb, rgbToHex };
