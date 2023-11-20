f9236e47991028975bdaa7cb3e1d16b020795acb
Caleb Hugo <calebjedhugo@gmail.com>
2023-11-20 17:26:17 -0600

    Better display for clicking

--- src/useGradiantFromColor/Graphs/Dot.js
+++ src/useGradiantFromColor/Graphs/Dot.js
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 const Dot = styled('div')`
   background-color: ${({ $color }) => $color};
   opacity: ${({ $opacity }) => $opacity};
-  width: 3px;
-  height: 3px;
+  width: 5px;
+  height: 5px;
   position: absolute;
   border-radius: 50%;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Dot;
